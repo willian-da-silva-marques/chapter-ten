8f5bdfe51e6ab39d2578efa438073962d0f3fd8b
willian.marques <willian.marques@CTNDELL23W10Y1.pampa.compasso>
2020-01-28 10:36:02 -0300

    ultimo commit

--- src/com/ocajexam/chapter/ten/TestLocalDateTime.java
+++ src/com/ocajexam/chapter/ten/TestLocalDateTime.java
@@ -1,15 +1,17 @@
 package com.ocajexam.chapter.ten;
 
 import java.time.LocalDateTime;
+import java.time.Period;
 
 public class TestLocalDateTime {
 
 	public static void main(String[] args) {
 		
-		LocalDateTime localDateTime = LocalDateTime.now();
-		System.out.println(localDateTime.plusDays(3));
-		System.out.println(localDateTime.toLocalDate().plusDays(3));
-		System.out.println(localDateTime.toLocalTime());
+		LocalDateTime startDateInclusive = LocalDateTime.of(1900,9,1,1,06,32,1000000);
+		System.out.println(startDateInclusive);
+		LocalDateTime endDateExclusive = LocalDateTime.now();
+		Period period = Period.between(startDateInclusive.toLocalDate(), endDateExclusive.toLocalDate());
+		
+		System.out.println("A diferença é de " + period.getYears() + " anos " + period.getMonths() + " mêses e " + period.getDays() + " dias.");
 	}
-
 }
